b8de3c105cc7fb38bba1a17bb7dffc3ba5d66b98
shaun <shaun@shauns-MacBook-Air.local>
2021-08-04 23:57:33 +0800

    Demo Streaming

--- cypress/integration/api/apitest.spec.js
+++ cypress/integration/api/apitest.spec.js
@@ -1,7 +1,7 @@
 
 describe('Api Endpoint Tests', () => {
   it('Visit home page', () => {
-    cy.visit('http://localhost:5000/')
+    cy.visit('https://eager-tereshkova-c3ca44.netlify.app/')
 
     cy.get('.home-title-div-title')
     .should('not.be.empty')
@@ -17,40 +17,40 @@ describe('Api Endpoint Tests', () => {
   })
 
   it('Check home page access', () => {
-    cy.visit('http://localhost:5000/')
+    cy.visit('https://eager-tereshkova-c3ca44.netlify.app/')
 
     cy.get('.home-title-div-title').should('not.be.empty').click()
 
     .then(() => {
       cy.wait(1000)
-      cy.url().should('eq', 'http://localhost:5000/')
+      cy.url().should('eq', 'https://eager-tereshkova-c3ca44.netlify.app/')
     })
   })
 
   it('Access series endpoint', () => {
-    cy.visit('http://localhost:5000/')
+    cy.visit('https://eager-tereshkova-c3ca44.netlify.app/')
 
     cy.get('.series-div').should('not.be.empty').click()
 
     .then(() => {
       cy.wait(1000)
-      cy.url().should('eq', 'http://localhost:5000/series')
+      cy.url().should('eq', 'https://eager-tereshkova-c3ca44.netlify.app/series')
     })
   })
 
   it('Access movies endpoint', () => {
-    cy.visit('http://localhost:5000/')
+    cy.visit('https://eager-tereshkova-c3ca44.netlify.app/')
 
     cy.get('.movies-div').should('not.be.empty').click()
 
     .then(() => {
       cy.wait(1000)
-      cy.url().should('eq', 'http://localhost:5000/movies')
+      cy.url().should('eq', 'https://eager-tereshkova-c3ca44.netlify.app/movies')
     })
   })
 
   it('Access random endpoint', () => {
-    cy.visit('http://localhost:5000/abc')
+    cy.visit('https://eager-tereshkova-c3ca44.netlify.app/abc')
 
     cy.get('.error-div').should('not.be.empty')
     .invoke('text')
